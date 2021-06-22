b3e68d614dd67bdcd1a1cf3940ed8eccabdc61c8
Jonathan-Odia Osahon Emmanuel <emmyodia@gmail.com>
2021-06-22 15:51:58 +0100

    security config class creaeted

---
+++ src/main/java/com/practice/invoicingapp/security/ApplicationSecurityConfig.java
@@ -0,0 +1,9 @@
+package com.practice.invoicingapp.security;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+
+}
