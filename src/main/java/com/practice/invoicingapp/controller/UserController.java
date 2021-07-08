04b3287ce3b0dda3424495789d2464792dc0098a
Jonathan-Odia Osahon Emmanuel <emmyodia@gmail.com>
2021-07-08 06:16:22 +0100

    authenticate end point created

--- src/main/java/com/practice/invoicingapp/controller/UserController.java
+++ src/main/java/com/practice/invoicingapp/controller/UserController.java
@@ -1,11 +1,19 @@
 package com.practice.invoicingapp.controller;
 
-import com.practice.invoicingapp.entities.Customer;
-import com.practice.invoicingapp.entities.Invoice;
-import com.practice.invoicingapp.entities.User;
+import com.practice.invoicingapp.entities.*;
+import com.practice.invoicingapp.security.UserDetailsServiceImp;
 import com.practice.invoicingapp.service.UserService;
 import com.practice.invoicingapp.service.UserServiceImpl;
+import com.practice.invoicingapp.util.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
@@ -13,10 +21,35 @@ import java.util.Set;
 
 @RestController
 public class UserController {
+
     private final UserService userService;
+
+    @Autowired
+    @Qualifier("userdetailsserviceimpl")
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+
+    AuthenticationManager authenticationManager;
 
     public UserController(UserServiceImpl userService) {
         this.userService = userService;
+    }
+
+
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws Exception{
+        try { authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(),authenticationRequest.getPassword()));}
+                catch(BadCredentialsException e) {
+                    throw new Exception("Incorrect username or password", e);
+                }
+                final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+                final String jwt = jwtUtil.generateToken(userDetails);
+                return ResponseEntity.ok(new AuthenticationResponse(jwt));
     }
 
     @GetMapping("/getByEmail/{email}")
